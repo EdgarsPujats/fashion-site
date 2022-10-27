9bfa5e8cf6e575774565a4e9d45d2c935e10d4eb
Edgars Pujats <edgars.pujats@magebit.com>
2022-10-27 17:48:19 +0300

    UK tax and custom 5% tax added

--- pub/static/frontend/Venustheme/fasony/en_US/requirejs-config.js
+++ pub/static/frontend/Venustheme/fasony/en_US/requirejs-config.js
@@ -526,6 +526,23 @@ require.config(config);
 var config = {
     map: {
         '*': {
+            captcha: 'Magento_Captcha/js/captcha',
+            'Magento_Captcha/captcha': 'Magento_Captcha/js/captcha'
+        }
+    }
+};
+
+require.config(config);
+})();
+(function() {
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+var config = {
+    map: {
+        '*': {
             configurable: 'Magento_ConfigurableProduct/js/configurable'
         }
     },
